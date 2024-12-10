d4003b796843290c41a31896ff79055d11750a1c
lh-25 <146130686+lh-25@users.noreply.github.com>
2024-12-10 11:35:33 -0600

    finished models, did migrations and added all files needed

--- sonora/settings.py
+++ sonora/settings.py
@@ -76,10 +76,11 @@ WSGI_APPLICATION = 'sonora.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sonora',
     }
 }
+
 
 
 # Password validation
@@ -117,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "main_app" / "static"]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "main_app" /  "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
