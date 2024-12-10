a768191ae9f6268b4137845a45775e441bd707cb
lh-25 <146130686+lh-25@users.noreply.github.com>
2024-12-10 14:57:12 -0600

    finished views/urls edited song model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Song, SongOfTheDay, Playlist
 
 # Register your models here.
+admin.site.register(Song),
+admin.site.register(SongOfTheDay),
+admin.site.register(Playlist)
