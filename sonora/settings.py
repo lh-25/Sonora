be49784b3e54d4d3763bc4c93b547c6b509d2afe
lh-25 <146130686+lh-25@users.noreply.github.com>
2024-12-12 11:23:17 -0600

    debugging deployment

--- sonora/settings.py
+++ sonora/settings.py
@@ -83,6 +83,7 @@ WSGI_APPLICATION = 'sonora.wsgi.application'
 if 'ON_HEROKU' in os.environ:
     DATABASES = {
         "default": dj_database_url.config(
+             env='DATABASE_URL',
             conn_max_age=600,
             conn_health_checks=True,
             ssl_require=True,
